3a4d2dfc12f2890535c6bb72630a3c3d86214a06
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-10-22 14:36:15 +0100

    :|

--- SecurityService.OpenIdConnect.IntegrationTests/Common/Hooks.cs
+++ SecurityService.OpenIdConnect.IntegrationTests/Common/Hooks.cs
@@ -65,7 +65,7 @@
         public async Task BeforeScenario()
         {
             String? browser = Environment.GetEnvironmentVariable("Browser");
-            browser = "Firefox";
+            //browser = "Firefox";
 
             if (browser == null || browser == "Chrome")
             {
