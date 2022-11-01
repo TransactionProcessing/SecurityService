ad650af500dd452186f8a4324aa15d8fcad710c0
Stuart Ferguson <stuart_ferguson1development@outlook.com>
2022-11-01 10:18:34 +0000

    try fix issue with ES starting on windows

--- SecurityService.IntegrationTests/Common/GenericSteps.cs
+++ SecurityService.IntegrationTests/Common/GenericSteps.cs
@@ -8,7 +8,68 @@ namespace SecurityService.IntergrationTests.Common
     using Shared.IntegrationTesting;
     using Shared.Logger;
     using System.Threading.Tasks;
+    using Ductus.FluentDocker.Services;
     using TechTalk.SpecFlow;
+    using Ductus.FluentDocker.Model.Builders;
+    using Ductus.FluentDocker.Services.Extensions;
+    using System.IO;
+    using Ductus.FluentDocker.Builders;
+
+    public class LocalDockerHelper : DockerHelper
+    {
+        public override async Task<IContainerService> SetupEventStoreContainer(INetworkService networkService,
+                                                                         Boolean isSecure = false) {
+            this.Trace("About to Start Event Store Container");
+
+            List<String> environmentVariables = new() {
+                                                      "EVENTSTORE_RUN_PROJECTIONS=all",
+                                                      "EVENTSTORE_START_STANDARD_PROJECTIONS=true",
+                                                      "EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP=true",
+                                                      "EVENTSTORE_ENABLE_EXTERNAL_TCP=true"
+                                                  };
+
+            String logfolder = "/var/log/eventstore";
+            DockerEnginePlatform enginePlatform = DockerHelper.GetDockerEnginePlatform();
+            if (enginePlatform == DockerEnginePlatform.Windows) {
+                logfolder = "C:\\Logs";
+            }
+
+            ContainerBuilder eventStoreContainerBuilder = new Builder().UseContainer().UseImageDetails(this.GetImageDetails(ContainerType.EventStore))
+                                                                       .ExposePort(DockerPorts.EventStoreHttpDockerPort).ExposePort(DockerPorts.EventStoreTcpDockerPort)
+                                                                       .WithName(this.EventStoreContainerName).UseNetwork(networkService)
+                                                                       .MountHostFolder(this.HostTraceFolder, logfolder);
+
+            if (isSecure == false)
+            {
+                environmentVariables.Add("EVENTSTORE_INSECURE=true");
+            }
+            else
+            {
+                // Copy these to the container
+                String path = Path.Combine(Directory.GetCurrentDirectory(), "certs");
+
+                eventStoreContainerBuilder = eventStoreContainerBuilder.Mount(path, "/etc/eventstore/certs", MountType.ReadWrite);
+
+                // Certificates configuration
+                environmentVariables.Add("EVENTSTORE_CertificateFile=/etc/eventstore/certs/node1/node.crt");
+                environmentVariables.Add("EVENTSTORE_CertificatePrivateKeyFile=/etc/eventstore/certs/node1/node.key");
+                environmentVariables.Add("EVENTSTORE_TrustedRootCertificatesPath=/etc/eventstore/certs/ca");
+            }
+
+            eventStoreContainerBuilder = eventStoreContainerBuilder.WithEnvironment(environmentVariables.ToArray());
+
+            IContainerService eventStoreContainer = eventStoreContainerBuilder.Build().Start();
+            await Retry.For(async () => { eventStoreContainer = eventStoreContainer.WaitForPort($"{DockerPorts.EventStoreHttpDockerPort}/tcp"); });
+
+            this.EventStoreHttpPort = eventStoreContainer.ToHostExposedEndpoint($"{DockerPorts.EventStoreHttpDockerPort}/tcp").Port;
+            this.Trace($"EventStore Http Port: [{this.EventStoreHttpPort}]");
+
+            this.Trace("Event Store Container Started");
+
+            this.Containers.Add(eventStoreContainer);
+            return eventStoreContainer;
+        }
+    }
 
     [Binding]
     [Scope(Tag = "base")]
@@ -34,13 +95,14 @@ namespace SecurityService.IntergrationTests.Common
             logger.Initialise(LogManager.GetLogger(scenarioName), scenarioName);
             LogManager.AddHiddenAssembly(typeof(NlogLogger).Assembly);
 
-            this.TestingContext.DockerHelper = new DockerHelper();
+            this.TestingContext.DockerHelper = new LocalDockerHelper();
             this.TestingContext.DockerHelper.Logger = logger;
             this.TestingContext.DockerHelper.SqlServerContainer = Setup.DatabaseServerContainer;
             this.TestingContext.DockerHelper.SqlServerNetwork = Setup.DatabaseServerNetwork;
             this.TestingContext.DockerHelper.DockerCredentials = Setup.DockerCredentials;
             this.TestingContext.DockerHelper.SqlCredentials = Setup.SqlCredentials;
             this.TestingContext.DockerHelper.SqlServerContainerName = "sharedsqlserver";
+            
 
             this.TestingContext.DockerHelper.SetImageDetails(ContainerType.SecurityService, ("securityservice", false));
             DockerEnginePlatform enginePlatform = DockerHelper.GetDockerEnginePlatform();
